babfced388f0dd0069de41e065be8f191c702c3c
efecan10 <250541093@firat.edu.tr>
2025-12-03 21:10:40 +0300

    Update Exercise4_4.java with comments and details
    Added comments and personal information to the class.

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,4 +1,7 @@
-public class Exercise4_4 {
+ad soyad : efecan önal
+numara :250541093
+
+    public class Exercise4_4 {
 
     public static int getNumber() {
         return 42;
@@ -12,10 +15,15 @@ public class Exercise4_4 {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
+        // CEVAP 1: Kod hatasiz calisir (Compiles and runs fine).
+        // Metot gorevini yapar ama donen 42 degeri havada kalir (bosa gider),
+        // cunku bir degiskene atamadik veya ekrana yazdirmadik.
+
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
-
-        // Cevaplarinizi yorum olarak ekleyin.
+        // CEVAP 2: Bu satir acilirsa DERLEME HATASI verir (Compiler Error).
+        // Cunku 'void' metotlar geriye bir sayi veya deger dondurmez.
+        // Hata: "operator + cannot be applied to void" (Void ile sayi toplanamaz).
     }
 }
